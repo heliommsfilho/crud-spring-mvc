62338bb40e26b461cf669e6a24484cf25a4ff495
Hélio Márcio Filho <helio@inforio.com.br>
2017-04-25 18:00:17 -0300

    Enviando alterações

--- titulos-cobranca/src/main/java/com/algaworks/titulos/cobranca/model/StatusTitulo.java
+++ titulos-cobranca/src/main/java/com/algaworks/titulos/cobranca/model/StatusTitulo.java
@@ -3,7 +3,8 @@ package com.algaworks.titulos.cobranca.model;
 public enum StatusTitulo {
 
 	PENDENTE("Pendente"),
-	RECEBIDO("Recebido");
+	RECEBIDO("Recebido"),
+	CANCELADO("Cancelado");
 	
 	private String descricao;
 	
